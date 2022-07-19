671673dabcc4bde06f526ed00334106b71412c9c
Christoph Heindl <cheind@profactor.at>
2022-07-19 13:46:21 +0200

    added quasi de bruijn test for sequences. strange for a4

--- neuraldot/anoto.py
+++ neuraldot/anoto.py
@@ -117,6 +117,14 @@ A quasi De Bruijn sequence of order 5 and length 241. In a quasi De Bruijn
 sequence of order n, each possible substring of length n appears _at most_
 once.
 
+TODO: This sequence is strange. According to the patent it has only 240 digits and
+not 241. However 241 are required for math.lcm(236,233,31,241) to be 410815348. In
+some codes (libdots) it seems like a 1 is appended to make it 241. However, that 
+breaks the quasi De Bruijn property and multiple 5 long sequences reappear (e.g
+[1 1 1 0 0] appears at pos 82 and 238 (cylcic) for more see the test). Best choice 
+would be to append a 0, which would give 237 unique substrings instead of 241. For 
+now I leave the 1 appended, since this is what most codes do.
+
 References:
 Anoto AB  "Method and device for decoding a position-coding pattern"
 https://patentimages.storage.googleapis.com/b8/ef/c2/046cdc9e044b9e/US7999798.pdf
@@ -126,19 +134,14 @@ Aboufadel, Edward, Timothy Armstrong, and Elizabeth Smietana.
 """
 A4 = np.array(
     [
-        0,0,0,0,0,1,0,2,0,0,0,0,2,0,0,2,0,1,0,
-        0,0,1,1,2,0,0,0,1,2,0,0,2,1,0,0,0,2,1,
-        1,2,0,1,0,1,0,0,1,2,1,0,0,1,0,0,2,2,0,
-        0,0,2,2,1,0,2,0,1,1,0,0,1,1,1,0,1,0,1,
-        1,0,1,2,0,1,1,1,1,0,0,2,0,2,0,1,2,0,2,
-        2,0,1,0,2,1,0,1,2,1,1,0,1,1,1,2,2,0,0,
-        1,0,1,2,2,2,0,0,2,2,2,0,1,2,1,2,0,2,0,
-        0,1,2,2,0,1,1,2,1,0,2,1,1,0,2,0,2,1,2,
-        0,0,1,1,0,2,1,2,1,0,1,0,2,2,0,2,1,0,2,
-        2,1,1,1,2,0,2,1,1,1,0,2,2,2,2,0,2,0,2,
-        2,1,2,1,1,1,1,2,1,2,1,2,2,2,1,0,0,2,1,
-        2,2,1,0,1,1,2,2,1,1,2,1,2,2,2,2,1,2,0,
-        1,2,2,1,2,2,0,2,2,2,1,1,1
+        0,0,0,0,0,1,0,2,0,0,0,0,2,0,0,2,0,1,0,0,0,1,1,2,0,0,0,
+        1,2,0,0,2,1,0,0,0,2,1,1,2,0,1,0,1,0,0,1,2,1,0,0,1,0,0,2,2,0,0,
+        0,2,2,1,0,2,0,1,1,0,0,1,1,1,0,1,0,1,1,0,1,2,0,1,1,1,1,0,0,2,0,
+        2,0,1,2,0,2,2,0,1,0,2,1,0,1,2,1,1,0,1,1,1,2,2,0,0,1,0,1,2,2,2,
+        0,0,2,2,2,0,1,2,1,2,0,2,0,0,1,2,2,0,1,1,2,1,0,2,1,1,0,2,0,2,1,
+        2,0,0,1,1,0,2,1,2,1,0,1,0,2,2,0,2,1,0,2,2,1,1,1,2,0,2,1,1,1,0,
+        2,2,2,2,0,2,0,2,2,1,2,1,1,1,1,2,1,2,1,2,2,2,1,0,0,2,1,2,2,1,0,
+        1,1,2,2,1,1,2,1,2,2,2,2,1,2,0,1,2,2,1,2,2,0,2,2,2,1,1,1
     ]
 )
 
--- tests/test_anoto.py
+++ tests/test_anoto.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pytest
+
 from neuraldot import anoto
 
 
@@ -7,3 +10,31 @@ def test_sequence_lengths():
     assert len(anoto.A2) == 233
     assert len(anoto.A3) == 31
     assert len(anoto.A4) == 241
+
+
+@pytest.mark.parametrize(
+    "seq,n,L",
+    [
+        (anoto.MNS, 6, 63),
+        (anoto.A1, 5, 236),
+        (anoto.A2, 5, 233),
+        (anoto.A3, 5, 31),
+        (anoto.A4, 5, 241),
+    ],
+)
+def test_sequence_quasi_debruijn(seq, n, L):
+    # make it cyclic
+    cseq = np.concatenate((seq, seq[: (n - 1)]))
+    view = np.lib.stride_tricks.sliding_window_view(cseq, n, axis=0)
+    assert view.shape == (L, n)
+    hashes = np.apply_along_axis(lambda w: hash(w.tobytes()), 1, view)
+
+    s = {}
+    for idx, (e, h) in enumerate(zip(view, hashes)):
+        if h in s:
+            print("collision")
+            print(idx, h, s[h], e)
+        else:
+            s[h] = (e, idx)
+
+    assert len(set(hashes)) == L
