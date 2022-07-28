9378ba06f3cefbf9bb6b885bc49278b9bc666842
Christoph Heindl <cheind@profactor.at>
2022-07-28 13:57:45 +0200

    added doc table generation

---
+++ doc/gen_tables.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from neuraldot import integer
+
+
+def generate_numbers_pfactor_basis():
+    nums = np.arange(12)
+    perms = [[2, 2, 3], [2, 3, 2], [3, 2, 2]]
+
+    data = {}
+
+    perm_coeffs = []
+    for pf in perms:
+        cname = f'[{",".join(str(p) for p in pf)}]'
+        coeffs = integer.NumberBasis(pf).project(nums)
+        str_coeffs = []
+        for cs in coeffs:
+            str_coeffs.append(f'{"".join([str(c) for c in cs])}')
+        data[cname] = str_coeffs
+
+    df = pd.DataFrame(data)
+    print(df.to_latex())
+
+
+generate_numbers_pfactor_basis()
