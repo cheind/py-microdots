67d4d8e0b7fd9b0d607c18b6832cf66d33d4f0ad
Christoph Heindl <christoph.heindl@gmail.com>
2022-08-05 16:30:10 +0200

    added detection rectangles

--- doc/gen_plots.py
+++ doc/gen_plots.py
@@ -1,19 +1,47 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as P
 from neuraldot.defaults import anoto_encoder_6x6_a4_fixed
 from neuraldot.draw import draw_dots
 
 
 def generate_pattern_plot():
-    bits = anoto_encoder_6x6_a4_fixed.encode_bitmatrix((6, 16), section=(0, 0))
-    fig, ax = plt.subplots(figsize=plt.figaspect(6 / 16))
+    bits = anoto_encoder_6x6_a4_fixed.encode_bitmatrix((8, 16), section=(0, 0))
+    fig, ax = plt.subplots(figsize=plt.figaspect(8 / 16))
+
+    draw_dots(bits, dot_scale=25, ax=ax)
+
+    ax.add_patch(
+        P.Rectangle(
+            (2 - 0.5, 1 - 0.5),
+            6,
+            6,
+            fill=False,
+            edgecolor="green",
+            zorder=3,
+            linewidth=2,
+        )
+    )
+    ax.add_patch(
+        P.Rectangle(
+            (7 - 0.5, 2 - 0.5),
+            6,
+            6,
+            fill=False,
+            edgecolor="purple",
+            zorder=3,
+            linewidth=2,
+        )
+    )
+    ax.annotate("(2,1)", (2 - 0.3, 1 - 0.15), color="green", fontsize=11)
+    ax.annotate("(7,2)", (7 - 0.3, 2 - 0.15), color="purple", fontsize=11)
+
     ax.spines["right"].set_visible(False)
     ax.spines["top"].set_visible(False)
     ax.spines["left"].set_visible(False)
     ax.spines["bottom"].set_visible(False)
     ax.tick_params("x", labelsize=12)
     ax.tick_params("y", labelsize=12)
-
-    draw_dots(bits, dot_scale=40, ax=ax)
+    ax.grid(which="major", alpha=0.4, linestyle="--", color="gray", zorder=0)
     fig.tight_layout()
     fig.savefig("doc/anoto_pattern.pdf")
     plt.show()
