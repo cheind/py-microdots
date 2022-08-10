d44bb5f059cabec4eb68fd73bcfc0c0890583e3a
Christoph Heindl <cheind@profactor.at>
2022-08-10 14:25:54 +0200

    new test, need voting

--- tests/test_codec.py
+++ tests/test_codec.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from neuraldot import defaults, codec
+from neuraldot import defaults, codec, helpers
 
 
 def test_bitmatrix_encode_different_sections():
@@ -72,23 +72,78 @@ def test_bit_packing():
     assert np.allclose(bits, rbits)
 
 
-# def test_bitmatrix_decode_orientation():
-#     anoto = defaults.anoto_6x6_a4_fixed
+def test_bitmatrix_decode_orientation():
+    anoto = defaults.anoto_6x6_a4_fixed
+    m = anoto.encode_bitmatrix((256, 256), section=(5, 10))
 
-#     m = anoto.encode_bitmatrix((256, 256), section=(5, 10))
+    def search_seqs(mat, idx=0):
+        mat = mat.astype(np.int8)
+        xcol = anoto.mns_cyclic_bytes.find(mat[:, idx, 0].tobytes())
+        ycol = anoto.mns_cyclic_bytes.find(mat[:, idx, 1].tobytes())
+        xrow = anoto.mns_cyclic_bytes.find(mat[idx, :, 0].tobytes())
+        yrow = anoto.mns_cyclic_bytes.find(mat[idx, :, 1].tobytes())
+        return xrow, yrow, xcol, ycol
 
-#     # for y in range(256 - 8):
-#     #     for x in range(256 - 8):
-#     #         s = m[y : y + 8, x : x + 8]
-#     #         for k in range(4):
-#     #             r = np.rot90(s, k=k, axes=(0, 1))
-#     #             kfix = anoto.decode_rotation(r)
-#     #             if k != ((4 - kfix) % 4):
-#     #                 print(y, x, k)
-#     #             assert k == ((4 - kfix) % 4)
+    for i in range(256 - 8):
+        for j in range(256 - 8):
+            s = m[i : i + 8, j : j + 8].copy()
+            print(i, j)
 
-#     s = m[34 : 34 + 8, 79 : 79 + 8]
-#     r = np.rot90(s, k=1, axes=(0, 1))
-#     kfix = anoto.decode_rotation(r)
-#     print(kfix)
-#     # assert k == ((4 - kfix) % 4)
+            xrow, yrow, xcol, ycol = search_seqs(s)
+            print(xrow, yrow, xcol, ycol)
+            assert yrow >= 0 and xcol >= 0
+
+            s = helpers.rot90_cw(s)
+            xrow, yrow, xcol, ycol = search_seqs(s)
+            print(xrow, yrow, xcol, ycol)
+            assert yrow == -1 and xcol >= 0
+
+            s = helpers.rot90_cw(s)
+            xrow, yrow, xcol, ycol = search_seqs(s)
+            print(xrow, yrow, xcol, ycol)
+            assert yrow == -1 and xcol == -1
+
+            print("-----")
+            s = helpers.rot90_cw(s)
+            xrow, yrow, xcol, ycol = search_seqs(s)
+            print(xrow, yrow, xcol, ycol)
+            xrow, yrow, xcol, ycol = search_seqs(s, 1)
+            print(xrow, yrow, xcol, ycol)
+            xrow, yrow, xcol, ycol = search_seqs(s, 2)
+            print(xrow, yrow, xcol, ycol)
+            xrow, yrow, xcol, ycol = search_seqs(s, 3)
+            print(xrow, yrow, xcol, ycol)
+            assert yrow >= 0 and xcol == -1
+
+    # mat = helpers.rot90_cw(s)
+    # t = mat[:8, :8]
+    # print_search(t, 90)
+
+    # mr = helpers.rot90_cw(mr)
+    # t = mr[:8, :8]
+    # print_search(t, 180)
+
+    # mr = helpers.rot90_cw(mr)
+    # t = mr[:8, :8]
+    # print_search(t, 270)
+
+    # print(s[:, 0, 0], t[:, 0, 0])
+    # print(s[:, 0, 1], t[:, 0, 1])
+    # print(s[0, :, 0], t[0, :, 0])
+    # print(s[0, :, 1], t[0, :, 1])
+
+    # for y in range(256 - 8):
+    #     for x in range(256 - 8):
+    #         s = m[y : y + 8, x : x + 8]
+    #         for k in range(4):
+    #             r = np.rot90(s, k=k, axes=(0, 1))
+    #             kfix = anoto.decode_rotation(r)
+    #             if k != ((4 - kfix) % 4):
+    #                 print(y, x, k)
+    #             assert k == ((4 - kfix) % 4)
+
+    # s = m[34 : 34 + 8, 79 : 79 + 8]
+    # r = np.rot90(s, k=1, axes=(0, 1))
+    # kfix = anoto.decode_rotation(r)
+    # print(kfix)
+    # assert k == ((4 - kfix) % 4)
