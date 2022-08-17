76a481898206c708cb49153c4ed1cec52c58d4a1
Christoph Heindl <cheind@profactor.at>
2022-08-17 11:37:09 +0200

    moved requirements, added setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+THISDIR = Path(__file__).parent
+
+
+def read_requirements(fname):
+    with open(THISDIR / "requirements" / fname, "r") as f:
+        return f.read().splitlines()
+
+
+core_required = read_requirements("requirements.txt")
+dev_required = read_requirements("dev-requirements.txt")
+
+main_ns = {}
+with open(THISDIR / "sdftoolbox" / "__version__.py") as ver_file:
+    exec(ver_file.read(), main_ns)
+
+setup(
+    name="sdftoolbox",
+    version=main_ns["__version__"],
+    description="A modern Python library to encode/decode with Anoto dot patterns.",
+    author="Christoph Heindl",
+    url="https://github.com/cheind/py-microdots",
+    license="MIT",
+    install_requires=core_required,
+    packages=find_packages(".", include="microdots*"),
+    include_package_data=True,
+    keywords="speckle pattern coding anoto dot encode decode",
+    extras_require={
+        "dev": dev_required,
+    },
+)
